90cb69f70e78c8dabc0aede23f68528a523b71c4
ym28-it <yutamuraya.dev@gmail.com>
2025-05-09 18:19:01 +0900

    fix backend

--- backend/app/routers/user/user.py
+++ backend/app/routers/user/user.py
@@ -12,7 +12,7 @@ router = APIRouter()
 
 
 @router.post("/users/create", response_model=user_schema.UserCreateResponse, status_code=status.HTTP_201_CREATED)
-async def create(user: user_schema.UserCreate, db: AsyncSession = Depends(get_db)):
+async def create_user(user: user_schema.UserCreate, db: AsyncSession = Depends(get_db)):
     done = await user_crud.get_user_by_email(db, user.email)
     if done is not None:
         raise HTTPException(status_code=400, detail="Email already registered")
@@ -30,7 +30,7 @@ async def read_user(user_id: uuid.UUID, db: AsyncSession = Depends(get_db)):
 
 
 @router.post("/users/login", response_model=user_schema.LogInResponse)
-async def log_in(login_info: user_schema.LogIn, db: AsyncSession = Depends(get_db)):
+async def login(login_info: user_schema.LogIn, db: AsyncSession = Depends(get_db)):
     user = await user_crud.get_user_by_email(db, login_info.email)
     if user is None or not verify_password(login_info.password, user.password):
         raise HTTPException(status_code=401, detail="Invalid email or password")
@@ -48,9 +48,9 @@ async def rename_user_name(user_id: uuid.UUID, new_name: user_schema.RenameUserN
 
 
 @router.delete("/users/{user_id}", response_model=None)
-async def delete_user(login_info: user_schema.LogIn, db: AsyncSession = Depends(get_db)):
-    user = await user_crud.get_user_by_email(db, login_info.email)
-    if user is None or not verify_password(login_info.password, user.password):
-        raise HTTPException(status_code=401, detail="Invalid email or password")
+async def delete_user(user_id: uuid.UUID, db: AsyncSession = Depends(get_db)):
+    user = await user_crud.get_user_by_id(db, user_id)
+    if user is None:
+        raise HTTPException(status_code=404, detail="User not found")
     
     return await user_crud.user_delete(db, user)
